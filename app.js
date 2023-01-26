1bd2928182bf6e3f61eadd31c07a8809176de529
Hugolino B. Guimarães <hugolino2609@gmail.com>
2023-01-26 14:48:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
 const button = document.querySelector(".btnRegister");
+const currentYear = document.querySelector(".newYear");
 
 button.addEventListener("click", add);
 form.addEventListener("change", save);
@@ -23,7 +24,14 @@ function save() {
   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
 }
 
+function currentFullYear() {
+  const newYear = new Date().getFullYear();
+  currentYear.textContent = newYear;
+
+}
+
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
 
 nlwSetup.setData(data);
 nlwSetup.load();
+currentFullYear();
